26dfab183fb27828f5450fc7221eb69d72b361f4
Alexander Kastil <alexander.kastil@integrations.at>
2024-11-07 10:06:11 +0100

    Add authentication and Microsoft Graph integration to CareerGenie

--- labs/05/02-custom-engine-agents/03-sso/CareerGenie/src/config.ts
+++ labs/05/02-custom-engine-agents/03-sso/CareerGenie/src/config.ts
@@ -7,7 +7,13 @@ const config = {
   azureOpenAIEmbeddingDeploymentName: process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME,
   azureSearchKey: process.env.AZURE_SEARCH_KEY,
   azureSearchEndpoint: process.env.AZURE_SEARCH_ENDPOINT,
-  indexName: process.env.INDEX_NAME
+  indexName: process.env.INDEX_NAME,
+  aadAppClientId: process.env.AAD_APP_CLIENT_ID,
+  aadAppClientSecret: process.env.AAD_APP_CLIENT_SECRET,
+  aadAppOauthAuthorityHost: process.env.AAD_APP_OAUTH_AUTHORITY_HOST,
+  aadAppTenantId: process.env.AAD_APP_TENANT_ID,
+  botDomain: process.env.BOT_DOMAIN,
+  aadAppOauthAuthority: process.env.AAD_APP_OAUTH_AUTHORITY
 };
 
 export default config;
